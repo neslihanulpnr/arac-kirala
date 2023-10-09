625f945a70a692346734b5e8c8548b399aafb137
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2023-10-09 17:01:24 +0300

    new

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-localStorage.setItem("")
+const almabirakma = document.getElementById("alma-birakma");
+   
+almabirakma.addEventListener("submit", function (e){
+
+
+
+    localStorage.setItem("alma");
+    localStorage.setItem("bırakma");
+    localStorage.setItem("tarih ve saat");
+});
+
